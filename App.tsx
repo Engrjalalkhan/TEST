97dc8238b5b9a4fc58a1e3565d6bb1bd1327653b
Jalal khan <jk9494173@gmail.com>
2024-11-29 05:45:26 -0800

    FIRST COMMIT

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import Contact from './Src/screens/Contact';
-import Projects from './Src/screens/Projects';
-import Testimonials from './Src/screens/Testimonials';
 import Technology from './Src/screens/Technology';
+import Contact from './Src/screens/Contact';
+
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Technology/>
+      <Contact/>
     </View>
   );
 };
