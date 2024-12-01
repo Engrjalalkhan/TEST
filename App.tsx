d2d52fcd17fbb995cd204279ec7c81e5118736e4
Jalal khan <jk9494173@gmail.com>
2024-12-02 00:55:30 +0500

    FIRTST COMMIT

--- App.tsx
+++ App.tsx
@@ -1,24 +1,35 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import MainStack from './Src/navigation/MainStack';
+import {NavigationContainer} from '@react-navigation/native'; // Import NavigationContainer
+import {createStackNavigator} from '@react-navigation/stack'; // Import Stack Navigator
+import Homescreen from './Src/screens/Homescreen';
+import About from './Src/screens/About';
+import Projects from './Src/screens/Projects';
+import Testimonials from './Src/screens/Testimonials';
+import SEO from './Src/screens/SEO';
+import Technology from './Src/screens/Technology';
+import Quality from './Src/screens/Quality';
+import Contact from './Src/screens/Contact';
 
+const Stack = createStackNavigator(); // Create a Stack navigator
 
-const App = () => {
+const MainStack = () => {
   return (
-    <View style={styles.container}>
-      <MainStack/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Homescreen} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Projects" component={Projects} />
+        <Stack.Screen name="Testimonials" component={Testimonials} />
+        <Stack.Screen name="SEO" component={SEO} />
+        <Stack.Screen name="Technology" component={Technology} />
+        <Stack.Screen name="Quality" component={Quality} />
+        <Stack.Screen name="Contact" component={Contact} />
+        {/* Add other screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default App;
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor:'white',
-  },
-});
+export default MainStack;
